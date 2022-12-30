ca14b5b06676bf1db114af5d14347b357a6858d3
Jetom88 <shinhro3o@gmail.com>
2022-12-30 21:58:09 +0900

    💡Feat: 인터셉터 추가 및 Main 페이지 구현중

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
-import React from 'react';
-import useStore from '@store/store';
+import React from "react";
+import useStore from "@store/store";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  const {bears, increasePopulation, decreasePopulation, removeAllBears} = useStore();
-  console.log(`bears: ${bears}`);
+  const { bears, increasePopulation, decreasePopulation, removeAllBears } =
+    useStore();
+
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <button onClick={increasePopulation}>+</button>
-      <button onClick={decreasePopulation}>-</button>
-      <button onClick={removeAllBears}>clear</button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
