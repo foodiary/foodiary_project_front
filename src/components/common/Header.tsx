c0d8b74891c1eebdb45ef216e331bc49e9afc8d0
nanajin <nmj5182@gmail.com>
2023-01-09 13:34:33 +0900

    💄Design: 헤더 아이콘 svg파일로 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,29 +1,35 @@
 import React, { useState } from 'react';
-import {BsSearch, BsPlusSquare} from 'react-icons/bs';
-import {VscBell} from 'react-icons/vsc';
-import {FaUserCircle, FaRegCompass} from 'react-icons/fa';
-import {HiOutlineUserCircle} from 'react-icons/hi';
-import {RiHome5Line} from 'react-icons/ri';
 import styles from '@styles/header.module.scss';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import home_icon from '../../img/home_icon.svg';
+import hot_icon from '../../img/hot_icon.svg';
+import explore_icon from '../../img/explore_icon.svg';
+import mypage_icon from '../../img/mypage_icon.svg';
 
 const Header = () => {
   const [user, setUser] = useState(true);
+  const {pathname} = useLocation();
+
   return (
       <div className={styles.header}>
         <div className={styles.profile}>
-          {!user? <button className={styles.login_btn}>LOGIN</button>:
+          {!user? 
+            <div className={styles.before_login}>
+              <Link to="/signup/agree" className={styles.signup_btn}>회원가입</Link>
+              <Link to="/login" className={styles.login_btn}>로그인</Link>
+            </div>:
             <div className={styles.after_login}>
-              <button className={styles.bell}><VscBell/></button> 
-              <div className={styles.user}><FaUserCircle/></div>
+              <div className={styles.user}>
+                <Link to="/mypage"><img src={mypage_icon} alt="프로필사진"/></Link>
+              </div>
             </div>
           }
         </div>
         <div className={styles.navbar}>
-          <Link to="/"><RiHome5Line/></Link>
-          <Link to="/"><FaRegCompass/></Link>
-          <Link to="/"><BsPlusSquare/></Link>
-          <Link to="/mypage"><HiOutlineUserCircle/></Link>
+          <Link to="/"><img src={home_icon} alt="홈" className={pathname==="/" ? "": styles.no_active}/></Link>  
+          <Link to="/"><img src={hot_icon} alt="랭킹" className={pathname==="/" ? "": styles.no_active}/></Link>
+          <Link to="/"><img src={explore_icon} alt="탐색" className={pathname==="/" ?  "": styles.no_active}/></Link>
+          <Link to="/mypage"><img src={mypage_icon} alt="마이페이지" className={pathname==="/mypage" ? "": styles.no_active}/></Link>
         </div>
         
       </div>
