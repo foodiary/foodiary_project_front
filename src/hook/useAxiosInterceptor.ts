dbc37dce00335c3ecb9427b530a340e310f5bf61
nanajin <nmj5182@gmail.com>
2023-03-13 21:41:00 +0900

    💄Design: 프로필 변경 css 수정, 코드 정리

--- src/hook/useAxiosInterceptor.ts
+++ src/hook/useAxiosInterceptor.ts
@@ -1,48 +1,9 @@
-import { useErrorStore } from '@store/errorStore';
 import { useLoadingStore } from '@store/loadingStore';
 import axiosConfig from '@utils/axiosConfig';
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 export const useAxiosInterceptor = ()=>{
   const setLoading = useLoadingStore(state=>state.setLoading);
- 
-  const getRefresh = async()=>{
-    const refreshToken = localStorage.getItem("refresh_token");
-    const headers = { Refresh: `${refreshToken}` };
-
-    await axios.get(`${process.env.REACT_APP_API_URL}/auth/reissue`, { headers })
-    .then(res=>{
-      console.log(res);
-      const data = res.data;
-    }).catch(err=>{
-      console.log(err);
-    });
-
-
-  // const getRefresh = async()=>{
-  //   const refreshToken = localStorage.getItem("refresh_token");
-  //   const headers = { Refresh: `${refreshToken}` };
-
-  //   await axiosConfig.get("/auth/reissue", { headers })
-  //   .then(res=>{
-  //     console.log(res);
-  //     const data = res.data;
-  //     const newAccessToken = data.data.accessToken;
-  //     localStorage.setItem("access_token", newAccessToken);
-
-  //   }).catch(err=>{
-  //     console.log(err);
-  //     // localStorage.clear();
-  //     // window.location.reload();
-  //   })
-
-    // const newAccessToken = data.data.accessToken;
-    // const newRefreshToken = data.data.refreshToken; 
-
-    // localStorage.setItem("access_token", newAccessToken);
-    // localStorage.setItem("refresh_token", newRefreshToken);
-  }
-  // }
 
   const errorHandler = async(err:AxiosError<any, any>)=>{ //res에서 넘어온 에러
     setLoading(false);
@@ -51,14 +12,6 @@ export const useAxiosInterceptor = ()=>{
     console.log(`인터셉트 에러: ${err}`);
     const refreshToken = localStorage.getItem("refresh_token");
 
-    // if(config?.url === '/auth/reissue'){
-    //   if(refreshToken){ //!refreshToken
-    //     console.log('자동로그아웃');
-    //     localStorage.clear();
-    //     window.location.reload();
-    //   }
-    // }
-    
     //액세스토큰 만료 시
     if (err.response?.status === 401 && config?.url !== '/auth/reissue') {
       console.log('401임')
@@ -99,26 +52,6 @@ export const useAxiosInterceptor = ()=>{
           localStorage.clear();
           window.location.reload();
         })
-        // const res = await axios.get(`${process.env.REACT_APP_API_URL}/auth/reissue`, { headers }); //refresh로 access 토큰 재발급
-        // const data = res.data;
-        // console.log(res); //응답일수도 에러일수도
-
-        // const newAccessToken = data.accessToken;
-        // const newRefreshToken = data.refreshToken;
-        // const refreshExpired = data.refreshTokenExpirationMinutes;
-        // const accessExpired = data.accessTokenExpirationMinutes;
-
-        // localStorage.setItem("access_token", newAccessToken);
-        // localStorage.setItem("refresh_token", newRefreshToken);
-        // localStorage.setItem("refresh_expired", refreshExpired);
-        // localStorage.setItem("access_expired", accessExpired);
-
-        // if(newAccessToken){
-        //   console.log('뉴액세스토큰받았다')
-        //   config!.headers = {
-        //     Authorization: `${newAccessToken}`,
-        //   };
-        // }
         return await axios(config!);
 
       } catch (err) {
@@ -149,7 +82,6 @@ export const useAxiosInterceptor = ()=>{
 
   const responseHandler = (response:AxiosResponse)=>{
     setLoading(false); //응답 후 로딩 끄기
-    // console.log("인터셉트 응답:" + response);
     const accessToken = response.data.accessToken;
     const refreshToken = response.data.refreshToken;
     const refreshExpired = response.data.refreshTokenExpirationMinutes;
